b2c1a99419b5bd3a5b411c4293b8b95a8f7414d7
Jarrod Luca <jarrodluca@gmail.com>
2017-12-08 03:59:38 -0500

    gif to make debuggin more bearable

--- config.js
+++ config.js
@@ -16,10 +16,13 @@ module.exports = {
       access_token : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjU5Y2FiZmE0MWFiM2FlMjliNzk5YzNkYiIsInJscyI6MTAwLCJybGgiOjEwMDAsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE1MDY0NjM1NjIsImV4cCI6MTUwNjQ2NzE2MiwianRpIjoiMWQ4YjVkNmI5YjdmNDVmZTc0YTJkYTY4MGQ2Y2U1ODJkMDcyZjU4NyJ9.PxQwHYfIbN9PuDrSziJY1uKYjOdGZnQ37JXp4K9MVew",
       refresh_token : 'b18d914f8b3a6753e57e2eabb1c819da5ccfee6d'
     },
+// root: payload that tierion sends when a block has been found on the network.
+    // it's tricky to test automatically on local.
+    // look at blockchain.info that updates with every block found
     root: 'https://tweet-chain.herokuapp.com/'
   },
 
   github: {
-    remote_url: 'https://github.com/oleh-kolinko/trumpTweets.git' //Repo that all new tweets will be pushed
+    remote_url: 'https://github.com/jsl15c/trumpTweets.git' //Repo that all new tweets will be pushed
   }
 };
